d30cb0705ca574f405edf12d3f626a93ec2f1d55
Kuncheria-KV <kuncheria@keyvalue.systems>
2023-11-29 16:31:46 +0530

    remove titles

--- packages/ui/src/Carousel/Carousel.tsx
+++ packages/ui/src/Carousel/Carousel.tsx
@@ -6,8 +6,6 @@ const slideList = Array.from({ length: 30 }).map((_, i) => {
   return {
     id: i,
     image: `https://picsum.photos/1440/2842?random=${i}`,
-    title: `This is the title! ${i + 1}`,
-    subtitle: `This is the subtitle ${i + 1}!`,
   }
 })
 function Slide({ data }) {
@@ -26,8 +24,6 @@ function Slide({ data }) {
         source={{ uri: data.image }}
         style={{ width: windowWidth , height: windowHeight * 0.5 }}
       ></Image>
-      <Text style={{ fontSize: 24 }}>{data.title}</Text>
-      <Text style={{ fontSize: 18 }}>{data.subtitle}</Text>
     </View>
   )
 }
