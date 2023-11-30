a0ed0e5ba2f9fba6072cb65df3e1f02fc039dcb6
Kuncheria-KV <kuncheria@keyvalue.systems>
2023-11-30 12:24:45 +0530

    Add product details route

---
+++ apps/next/pages/products/[id]/index.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+import { ProductDetailScreen } from 'app/features/products/detail-screen'
+
+export default ProductDetailScreen
--- packages/app/features/products/detail-screen.tsx
+++ packages/app/features/products/detail-screen.tsx
@@ -6,7 +6,7 @@ import { useLink } from 'solito/link'
 
 const { useParam } = createParam<{ id: string }>()
 
-export function UserDetailScreen() {
+export function ProductDetailScreen() {
   const [id] = useParam('id')
   const media = useMedia()
   const { width: windowWidth } = useWindowDimensions()
