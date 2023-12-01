7ffbb2fcb9003a66ccfa40efbab2bcbd0364dffa
Kuncheria-KV <kuncheria@keyvalue.systems>
2023-12-01 15:10:50 +0530

    add recommendations

--- packages/app/features/products/detail-screen.tsx
+++ packages/app/features/products/detail-screen.tsx
@@ -13,12 +13,16 @@ import {
   YStack,
   useMedia,
   useWindowDimensions,
+  View,
+  ScrollView,
 } from '@my/ui'
 import { ColorSelector } from '@my/ui'
+import { ProductCard } from '@my/ui/src'
 import { ChevronLeft } from '@tamagui/lucide-icons'
 import React from 'react'
 import { createParam } from 'solito'
 import { useLink } from 'solito/link'
+import Recommendations from './Recomendations'
 
 const { useParam } = createParam<{ id: string }>()
 
@@ -29,33 +33,36 @@ export function ProductDetailScreen() {
   const link = useLink({
     href: '/',
   })
-  console.log({ size: media.sm })
 
   return (
-    <YStack>
-      <XStack jc="space-around">
-        <Stack>
-          <Carousel.Container width={media.sm ? windowWidth : 800} height={media.sm ? 200 : 400}>
-            <Carousel.Slider />
-          </Carousel.Container>
+    <ScrollView>
+      <YStack>
+        <Stack jc="space-around" flexDirection={media.sm ? 'column' : 'row'}>
+          <View height={media.sm ? 300 : 400}>
+            <Carousel.Container width={media.sm ? windowWidth : 800} height={media.sm ? 300 : 400}>
+              <Carousel.Slider />
+            </Carousel.Container>
+          </View>
+          <YStack padding="$4">
+            <H1>NIKE Jordans</H1>
+            <H4>Basketball Shoe</H4>
+            <H2 marginTop={24}>MRP: 21,000</H2>
+            <Text>incl. of taxes</Text>
+            <Text>(Also includes all applicable duties)</Text>
+
+            <ColorSelector colors={['red', 'blue', 'green', 'yellow']} marginTop="$5" />
+
+            <Paragraph width={media.sm ? windowWidth - 20 : 600} marginTop={24}>
+              Zion's third signature shoe celebrates the dedication he puts in to crafting his
+              singular game. Packed with court-ready tech, it's designed for ballers who are masters
+              of both land and air. Stay low and contained, then explode skywards, with a cushioned
+              return to earth. The Zion 3 helps hold you down—so you can fly your own way.
+            </Paragraph>
+          </YStack>
         </Stack>
-        <YStack >
-          <H1>NIKE Jordans</H1>
-          <H4>Basketball Shoe</H4>
-          <H2 marginTop={24}>MRP: 21,000</H2>
-          <Text>incl. of taxes</Text>
-          <Text>(Also includes all applicable duties)</Text>
-
-          <ColorSelector colors={['red', 'blue', 'green', 'yellow']} marginTop="$5" />
-
-          <Paragraph width={600} marginTop={24}>
-            Zion's third signature shoe celebrates the dedication he puts in to crafting his
-            singular game. Packed with court-ready tech, it's designed for ballers who are masters
-            of both land and air. Stay low and contained, then explode skywards, with a cushioned
-            return to earth. The Zion 3 helps hold you down—so you can fly your own way.
-          </Paragraph>
-        </YStack>
-      </XStack>
-    </YStack>
+        <H3 paddingLeft="$4" marginTop="$4">Recommendations:</H3>
+        <Recommendations horizontal marginTop="$4" marginBottom="$8"/>
+      </YStack>
+    </ScrollView>
   )
 }
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,5 +3,6 @@ export * from '@tamagui/toast'
 export * from './MyComponent'
 export * from './Carousel/Carousel';
 export * from './ColorSelector/ColorSelector';
+export * from './ProductCard/ProductCard'
 export { config } from './tamagui.config'
 export * from './CustomToast'
