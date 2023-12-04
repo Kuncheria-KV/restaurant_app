f9d1d41ddf136e3ade87b8b8d5afb87acd755041
Abhay <abhay@keyvalue.systems>
2023-12-04 13:37:15 +0530

    feat: product listing card implemented

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,5 +4,6 @@ export * from './MyComponent'
 export * from './Carousel/Carousel';
 export * from './ColorSelector/ColorSelector';
 export * from './ProductCard/ProductCard'
+export * from './ProductListCard/ProductListCard'
 export { config } from './tamagui.config'
 export * from './CustomToast'
